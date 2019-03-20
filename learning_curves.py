6583ba43f803d3b85827faf32b5e91284f9a99b4
Arlie Coles <arlie.coles@mail.mcgill.ca>
2019-03-20 14:12:41 -0400

    Learning curve plotting
    - epochs and walltime per experiment
    - epochs and walltime per optimizer
    - epochs and walltime per architecture

--- learning_curves.py
+++ learning_curves.py
@@ -8,6 +8,7 @@ import numpy as np
 import argparse
 import os
 
+
 def get_walltimes(log_path):
     # Walltime is the cumulative sum of time each epoch takes
     with open(log_path, 'r') as fp:
@@ -23,6 +24,7 @@ def get_walltimes(log_path):
             walltimes.append(time)
     return walltimes
 
+
 def gen_train_valid_ppl_epoch(x, save_dir):
     train_ppls = x['train_ppls']
     valid_ppls = x['valid_ppls']
@@ -36,6 +38,7 @@ def gen_train_valid_ppl_epoch(x, save_dir):
     plt.savefig(os.path.join(save_dir, 'train_valid_ppl_epoch.png'))
     plt.clf()   # Clear
 
+
 def gen_train_valid_ppl_walltime(x, walltimes, save_dir):
     train_ppls = x['train_ppls']
     valid_ppls = x['valid_ppls']
@@ -47,6 +50,7 @@ def gen_train_valid_ppl_walltime(x, walltimes, save_dir):
     plt.legend()
     plt.savefig(os.path.join(save_dir, 'train_valid_ppl_walltime.png'))
     plt.clf()   # Clear
+
 
 def gen_valid_arch_ppl_epoch(arch_ppls, save_dir):
     for arch, xs in arch_ppls.items():
@@ -60,7 +64,44 @@ def gen_valid_arch_ppl_epoch(arch_ppls, save_dir):
         plt.savefig(os.path.join(save_dir, '{}_ppl_epoch.png'.format(arch)))
         plt.clf()   # Clear
 
-def gen_valid_arch_ppl_walltime(arch_ppls, save_dir):
+
+def gen_valid_arch_ppl_walltime(arch_ppls, arch_walltimes, save_dir):
+    for arch, xs in arch_ppls.items():
+        for i, x in enumerate(xs):
+            valid_ppls = x['valid_ppls']
+            walltimes = arch_walltimes[arch][i]
+            plt.plot(walltimes, valid_ppls, color='blue')
+        plt.title('Perplexity over wall-clock-time')
+        plt.xlabel('Wall-clock-time (s)')
+        plt.ylabel('Perplexity (PPL)')
+        plt.savefig(os.path.join(save_dir, '{}_ppl_epoch.png'.format(arch)))
+        plt.clf()   # Clear
+
+
+def gen_valid_opt_ppl_epoch(opt_ppls, save_dir):
+    for opt, xs in opt_ppls.items():
+        for x in xs:
+            valid_ppls = x['valid_ppls']
+            epochs = range(len(valid_ppls) - 1)
+            plt.plot(epochs, valid_ppls, color='blue')
+        plt.title('Perplexity over epochs')
+        plt.xlabel('Epoch')
+        plt.ylabel('Perplexity (PPL)')
+        plt.savefig(os.path.join(save_dir, '{}_ppl_epoch.png'.format(opt)))
+        plt.clf()   # Clear
+
+
+def gen_valid_opt_ppl_walltime(opt_ppls, opt_walltimes, save_dir):
+    for opt, xs in opt_ppls.items():
+        for i, x in enumerate(xs):
+            valid_ppls = x['valid_ppls']
+            walltimes = opt_walltimes[arch][i]
+            plt.plot(walltimes, valid_ppls, color='blue')
+        plt.title('Perplexity over wall-clock-time')
+        plt.xlabel('Wall-clock-time (s)')
+        plt.ylabel('Perplexity (PPL)')
+        plt.savefig(os.path.join(save_dir, '{}_ppl_epoch.png'.format(opt)))
+        plt.clf()   # Clear
 
 
 if __name__ == '__main__':
@@ -71,6 +112,11 @@ if __name__ == '__main__':
                         help='The plots you want to generate. Possible values are: {}'.format(tasks))
     args = parser.parse_args()
 
+    # For collection
+    arch_ppls, arch_walltimes = {}, {}
+    opt_ppls, opt_walltimes = {}, {}
+
+    # Walk through experiment result directories
     subdirs = next(os.walk('.'))[1]
     for subdir in subdirs:
         if 'exp' in subdir:
@@ -94,26 +140,34 @@ if __name__ == '__main__':
             # Generate one validation PPL plot over epochs and walltime over all experiments per each architecture.
             # Do this by first accumulating the data for each arch, then do the plot outside this main if statement.
             elif args.task == 'architecture':
-                arch_ppls = {}
+                # Collect validation ppls
                 config_path = os.path.join(subdir, 'exp_config.txt')
                 with open(config_path, 'r') as fp:
                     config = fp.readlines()
-                arch = config[9].split('\t')[-1].lower()    # This is the line where "model" is in the config
+                arch = config[9].split('\t')[-1].strip().lower()    # This is the line where "model" is in the config
                 lc_path = os.path.join(subdir, 'learning_curves.py')
                 x = np.load(lc_path)[()]
                 if arch not in arch_ppls:
-                    arch_ppls[arch] = x
+                    arch_ppls[arch] = [x]
                 else:
                     arch_ppls[arch].append(x)
+
+                # Collect walltimes
+                log_path = os.path.join(subdir, 'log.txt')
+                walltimes = get_walltimes(log_path)
+                if arch not in arch_walltimes:
+                    arch_walltimes[arch] = [walltimes]
+                else:
+                    arch_walltimes[arch].append(walltimes)
 
             # Generate one validation PPL plot over epochs and walltime over all experiments per each optimizer.
             # Do this by first accumulating the data for each arch, then do the plot outside this main if statement.
             elif args.task == 'optimizer':
-                opt_ppls = {}
+                # Collect validation ppls
                 config_path = os.path.join(subdir, 'exp_config.txt')
                 with open(config_path, 'r') as fp:
                     config = fp.readlines()
-                opt = config[12].split('\t')[-1].lower()    # This is the line where "optimizer" is in the config
+                opt = config[12].split('\t')[-1].strip().lower()    # This is the line where "optimizer" is in the config
                 lc_path = os.path.join(subdir, 'learning_curves.py')
                 x = np.load(lc_path)[()]
                 if opt not in opt_ppls:
@@ -121,6 +175,20 @@ if __name__ == '__main__':
                 else:
                     opt_ppls[arch].append(x)
 
-    # Generate plots for architecture 
+                # Collect walltimes
+                log_path = os.path.join(subdir, 'log.txt')
+                walltimes = get_walltimes(log_path)
+                if opt not in opt_walltimes:
+                    opt_walltimes[opt] = [walltimes]
+                else:
+                    opt_walltimes[opt].append(walltimes)
+
+    # Generate plots for architecture
     if args.task == 'architecture':
         gen_valid_arch_ppl_epoch(arch_ppls, '.')
+        gen_valid_arch_ppl_walltime(arch_ppls, arch_walltimes, '.')
+
+    # Generate plots for optimizer
+    elif args.task == 'optimizer':
+        gen_valid_opt_ppl_epoch(opt_ppls, '.')
+        gen_valid_opt_ppl_epoch(opt_ppls, opt_walltimes, '.')
